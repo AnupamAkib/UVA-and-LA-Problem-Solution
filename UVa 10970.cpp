beadefb157b96c4c355776ab40787e7a20dfe736
Mir Anupam Hossain Akib <mirakib25@gmail.com>
2019-11-20 23:23:58 +0600

    Create UVa 10970.cpp

---
+++ UVa 10970.cpp
@@ -0,0 +1,17 @@
+/*BISMILLAHIR RAHMANIR RAHIM*/
+#include<bits/stdc++.h>
+#define case(res) printf("Case %d: %d\n", i, res)
+#define PI acos(-1)
+#define lli long long int
+#define loop(a, n, x) for(int i=a; i<=n; i+=x)
+#define npoint(x) fixed << setprecision(x)
+/*-----------------ANUPAM AKIB-------------------*/
+
+using namespace std;
+int main(){
+    int m, n;
+    while(scanf("%d %d", &m, &n)!=EOF){
+        cout << (m*n)-1 << endl;
+    }
+    return 0;
+}
