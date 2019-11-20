a1e1f2296b357c5787a1f30e83062393bdbebf4f
Mir Anupam Hossain Akib <mirakib25@gmail.com>
2019-11-20 23:33:03 +0600

    Create UVa 673.cpp

---
+++ UVa 673.cpp
@@ -0,0 +1,63 @@
+/*BISMILLAHIR RAHMANIR RAHIM*/
+#include<bits/stdc++.h>
+#include<string>
+#define lli long long int
+#define case(ans) printf("Case %d: %d\n", k, ans)
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define check cout << "\tOK" << endl
+#define PI acos(-1.0)
+#define loop(a, n, x) for(int i=a; i<=n; i+=x)
+#define npoint(x) fixed << setprecision(x)
+#define ALL(x) x.begin(), x.end()
+#define rev(x) reverse(x.begin(), x.end())
+#define pb(x) push_back(x)
+#define ppb pop_back()
+#define pf(x) push_front(x)
+#define ppf pop_front()
+#define mii map<int, int>
+#define mci map<char, int>
+#define mib map<int, bool>
+#define dqi deque<int>
+#define maxiii(a, b, c) max(max(a, b), c)
+#define miniii(a, b, c) min(min(a, b), c)
+#define vec vector<int>
+#define pr pair<int, int>
+/*-----------------ANUPAM AKIB-------------------*/
+
+using namespace std;
+
+//map<int, int> mark;
+
+int main(){
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    string str;
+    int t;
+    cin >> t;
+    cin.ignore();
+    while(t--){
+        getline(cin, str);
+        for(int i=0; i<str.size(); i++){
+            if(str[i]=='(' && str[i+1]==')'){
+                str.erase(str.begin()+i);
+                str.erase(str.begin()+i);
+                i=-1;
+            }
+            else if(str[i]=='[' && str[i+1]==']'){
+                str.erase(str.begin()+i);
+                str.erase(str.begin()+i);
+                i=-1;
+                //cout << str << " ";
+            }
+        }
+        //cout << str.size() << " ";
+        if(str.size()==0){
+            yes;
+        }
+        else{
+            no;
+        }
+    }
+    return 0;
+}
