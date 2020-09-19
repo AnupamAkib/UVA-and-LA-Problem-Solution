c12d67a18e06df39057b644c2bf9bbc7713276c3
Mir Anupam Hossain Akib <mirakib25@gmail.com>
2020-09-19 20:19:08 +0600

    Create UVa 10340.cpp

---
+++ UVa 10340.cpp
@@ -0,0 +1,77 @@
+/*BISMILLAHIR RAHMANIR RAHIM*/
+#include<bits/stdc++.h>
+#define lli long long int
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ALL(x) x.begin(), x.end()
+#define print(x) cout<<x<<endl;
+#define PI acos(-1.0)
+#define nl cout<<"\n"
+#define mem(ar, val) memset(ar, val, sizeof(ar))
+#define point(x) fixed<<setprecision(x)
+#define printcase(n) cout << "Case " << tc << ": " << n <<endl
+#define printcaseii(n, m) cout << "Case " << tc << ": " << n << " " << m <<endl
+#define aInput(ar, n) for(int i=0; i<n; i++)cin>>ar[i];
+#define vInput(v, n) for(int i=0; i<n; i++){lli x;cin>>x;v.push_back(x);}
+#define input2D(ar, row, col) for(int i=0; i<row; i++){for(int j=0; j<col; j++){cin >> ar[i][j];}}
+#define print2D(ar, row, col) for(int i=0; i<row; i++){for(int j=0; j<col; j++){cout << ar[i][j] << " ";}cout<<endl;}
+#define vprint(vec) for(int i=0; i<vec.size(); i++){cout << vec[i]; (i==vec.size()-1? cout<<endl : cout<<" ");}
+#define aprint(ar, n) for(int i=0; i<n; i++){cout << ar[i] << " ";}cout<<endl
+#define ANUPAM_AKIB ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define execution_time cerr<<endl;int z=30;while(z--){cerr<<'-';}cerr<<endl;cerr<<point(16)<<"**Time -> "<<(double)(clock()-tStart)/CLOCKS_PER_SEC<<"fs"<<endl;
+#define INPUT freopen("input.txt", "r", stdin)
+#define OUTPUT freopen("output.txt", "w", stdout)
+#define yo(x) cerr<<(#x)<<" = "<<(x)<<endl;
+#define ff first
+#define ss second
+using namespace std;
+const lli inf = INT_MAX;
+const int mod = 20000;
+lli findLCM(lli a, lli b){return ((a*b)/__gcd(a,b));}
+lli input(){lli x; cin >> x; return x;}
+int digitCNT(int n){return ceil(log10(n)+0.00000001);}
+bool cmp(lli a, lli b){return a>b;}
+
+void solve(string s, string t){
+    int k = 0;
+    for(int i=0; i<s.size(); i++){
+        bool paisi =false;
+        for(int j=k; j<t.size(); j++){
+            if(t[j]==s[i]){
+                k = j + 1;
+                paisi = true;
+                break;
+            }
+        }
+        if(!paisi){
+            print("No")
+            return;
+        }
+    }
+    print("Yes")
+}
+
+int main(){
+    ANUPAM_AKIB;
+    #ifndef ONLINE_JUDGE
+        clock_t tStart = clock();
+        INPUT;
+        //OUTPUT;
+    #endif
+
+//    int tc = 1;
+//    cin >> tc; //TEST CASE
+//    for(int i=1; i<=tc; i++){
+//        solve(i);
+//    }
+
+    string s, t;
+    while(cin >> s >> t){
+        solve(s, t);
+    }
+
+    #ifndef ONLINE_JUDGE
+        execution_time;
+    #endif
+    return EXIT_SUCCESS;
+}
