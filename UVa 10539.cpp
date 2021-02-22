5639c68debea2c501d62b749e103289ea91d603e
Mir Anupam Hossain Akib <mirakib25@gmail.com>
2021-02-22 22:39:25 +0600

    Create UVa 10539.cpp

---
+++ UVa 10539.cpp
@@ -0,0 +1,111 @@
+/*BISMILLAHIR RAHMANIR RAHIM*/
+#include<bits/stdc++.h>
+#define lli long long int
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+#define ALL(x) x.begin(), x.end()
+#define print(x) cout<<x<<endl;
+#define PI acos(-1.0)
+#define nl cout<<"\n"
+#define mem(ar, val) memset(ar, val, sizeof(ar))
+#define point(x) fixed<<setprecision(x)
+#define printcase(n) cout << "Case " << tc << ": " << n <<endl;
+#define printcaseii(n, m) cout << "Case " << tc << ": " << n << " " << m <<endl;
+#define aInput(ar, n) for(int i=0; i<n; i++)cin>>ar[i];
+#define vInput(v, n) for(int i=0; i<n; i++){lli xwq;cin>>xwq;v.push_back(xwq);}
+#define input2D(ar, row, col) for(int i=0; i<row; i++){for(int j=0; j<col; j++){cin >> ar[i][j];}}
+#define print2D(ar, row, col) for(int i=0; i<row; i++){for(int j=0; j<col; j++){cout << ar[i][j] << " ";}cout<<endl;}
+#define vprint(vec) for(int i=0; i<vec.size(); i++){cout << vec[i]; (i==vec.size()-1? cout<<endl : cout<<" ");}
+#define aprint(ar, n) for(int i=0; i<n; i++){cout << ar[i] << " ";}cout<<endl
+#define ANUPAM_AKIB ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define execution_time cerr<<endl;int z=30;while(z--){cerr<<'-';}cerr<<endl;cerr<<point(16)<<"**Time -> "<<(double)(clock()-tStart)/CLOCKS_PER_SEC<<"fs"<<endl;
+#define INPUT freopen("input.txt", "r", stdin)
+#define OUTPUT freopen("output.txt", "w", stdout)
+#define umap unordered_map
+#define IN input()
+#define yo(x) cout<<(#x)<<" = "<<(x)<<endl;
+#define ff first
+#define ss second
+#define VEC vector<lli>
+#define sz size()
+using namespace std;
+const lli inf = INT_MAX;
+const int mod = 100000;
+lli findLCM(lli a, lli b){return ((a*b)/__gcd(a,b));}
+lli input(){lli x; cin >> x; return x;}
+int digitCNT(int n){return ceil(log10(n)+0.00000001);}
+string to_binary(lli num){if(num==0) return ""; return to_binary(num/2)+to_string(num%2);}
+bool isPalindrome(string s){for(int i=0, j=s.size()-1; i<s.size()/2; i++, j--){if(s[i]!=s[j]) return 0;}return 1;}
+bool isPrime(lli n){if(n<2){return false;}if(n==2||n==3){return true;}if(n%2==0){return false;}for(lli i=3; i*i<=n; i+=2){if(n%i==0){return false;}}return true;}
+bool cmp(int a, int b){
+    return a>b;
+}
+
+int NUM = 1000005;
+bool mark[1000005];
+vector<lli> prime;
+
+void sieve(){
+    //true = not prime
+    //false = prime
+    for(int i=4; i<=NUM; i+=2){
+        mark[i]=true;
+    }
+    for(int i=3; i*i<=NUM; i+=2){
+        if(mark[i]==false){
+            for(int j=i*i; j<=NUM; j+=(i+i)){
+                mark[j]=true;
+            }
+        }
+    }
+    for(int i=2; i<=NUM; i++){
+        if(mark[i]==false){
+            prime.push_back(i);
+        }
+    }
+}
+
+vector<lli> v;
+
+void precal(){
+    for(auto p : prime){
+        if(p*p > 1e12) break;
+        for(lli i=p*p; i<=1e12; i*=p){
+            v.push_back(i);
+        }
+    }
+    sort(ALL(v));
+    //yo(v.sz)
+    //vprint(v)
+}
+
+void solve(lli tc){
+    lli l = IN, r = IN;
+    int a = lower_bound(ALL(v), l) - v.begin();
+    int b = upper_bound(ALL(v), r) - v.begin() - 1;
+    print(b-a+1)
+}
+
+int main(){
+    ANUPAM_AKIB;
+    #ifndef ONLINE_JUDGE
+        clock_t tStart = clock();
+        INPUT;
+        OUTPUT;
+    #endif
+
+    sieve();
+    precal();
+
+    lli tc = 1;
+    cin >> tc; //TEST CASE
+    for(int i=1; i<=tc; i++){
+        solve(i);
+    }
+    //while(cin >> tc) solve(tc);
+
+    #ifndef ONLINE_JUDGE
+        execution_time;
+    #endif
+    return EXIT_SUCCESS;
+}
