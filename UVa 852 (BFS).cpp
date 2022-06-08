030d99a5583aeeac1f82cf4ed29f6dac40d2d8ca
Mir Anupam Hossain Akib <mirakib25@gmail.com>
2022-06-08 17:50:06 +0600

    Create UVa 852 (BFS).cpp

---
+++ UVa 852 (BFS).cpp
@@ -0,0 +1,126 @@
+/*BISMILLAHIR RAHMANIR RAHIM*/
+#include<bits/stdc++.h>
+#define lli long long int
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+#define ALL(x) x.begin(), x.end()
+#define PI acos(-1.0)
+#define nl cout<<"\n"
+#define print(x) cout<<x<<endl;
+#define mem(ar, val) memset(ar, val, sizeof(ar))
+#define point(x) fixed<<setprecision(x)
+#define printcase(n) cout << "Case " << tc << ": " << n <<endl;
+#define printcaseii(n, m) cout << "Case " << tc << ": " << n << " " << m <<endl;
+#define aInput(ar, n) for(int i=0; i<n; i++)cin>>ar[i];
+#define vInput(v, n) for(int i=0; i<n; i++){lli xwq;cin>>xwq;v.push_back(xwq);}
+#define input2D(ar, row, col) for(int i=0; i<row; i++){for(int j=0; j<col; j++){cin >> ar[i][j];}}
+#define print2D(ar, row, col) for(int i=0; i<row; i++){for(int j=0; j<col; j++){cout << ar[i][j] << " ";}cout<<endl;}
+#define vprint(vec) for(int i=0; i<vec.size(); i++){cout << vec[i]; (i==vec.size()-1? cout<<endl : cout<<" ");}
+#define aprint(ar, n) for(int i=0; i<n; i++){cout << ar[i] << " ";}cout<<endl
+#define ANUPAM_AKIB ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define execution_time cerr<<endl;int z=30;while(z--){cerr<<'-';}cerr<<endl;cerr<<point(16)<<"**Time -> "<<(double)(clock()-tStart)/CLOCKS_PER_SEC<<"fs"<<endl;
+#define INPUT freopen("input.txt", "r", stdin)
+#define OUTPUT freopen("output.txt", "w", stdout)
+#define umap unordered_map
+#define _IN input()
+#define IN input()
+#define yo(x) cout<<(#x)<<" = "<<(x)<<endl;
+#define loop(n) for(int ctrx=0; ctrx<n; ctrx++)
+#define ff first
+#define ss second
+#define VEC vector<lli>
+#define sz size()
+using namespace std;
+const lli inf = 1e9+9;
+const lli mod = 4294967296;
+lli findLCM(lli a, lli b){return ((a*b)/__gcd(a,b));}
+lli input(){lli x; cin >> x; return x;}
+int digitCNT(lli n){int cnt = 0;while(n>0){cnt++;n/=10;}return cnt;}
+string to_binary(lli num){if(num==0) return ""; return to_binary(num/2)+to_string(num%2);}
+lli to_decimal(string s){lli r = 0, p = 1;for(int i=s.sz-1; i>=0; i--){if(s[i] == '1') r += p; p *= 2;}return r;}
+bool isPalindrome(string s){for(int i=0, j=s.size()-1; i<s.size()/2; i++, j--){if(s[i]!=s[j]) return 0;}return 1;}
+bool isPrime(lli n){if(n<2){return false;}if(n==2||n==3){return true;}if(n%2==0){return false;}for(lli i=3; i*i<=n; i+=2){if(n%i==0){return false;}}return true;}
+bool cmp(pair<lli, lli> a, pair<lli, lli> b){
+    return a.ff>b.ff;
+}
+
+char board[10][10];
+bool vis[10][10];
+
+int dx[] = {0, 0, -1, 1};
+int dy[] = {1, -1, 0, 0};
+
+pair<int, int> BFS(int i, int j){
+    int white = 0, black = 0;
+    vis[i][j] = true;
+    queue<pair<int, int> > Q;
+    Q.push({i, j});
+    while(!Q.empty()){
+        pair<int, int> h = Q.front();
+        Q.pop();
+        for(int k=0; k<4; k++){
+            int x = h.ff + dx[k];
+            int y = h.ss + dy[k];
+            //print(x << " " << y)
+            if(x>=0 and x<9 and y>=0 and y<9){
+                if(board[x][y]=='.' and !vis[x][y]){
+                    vis[x][y] = true;
+                    Q.push({x, y});
+                }
+                else if(board[x][y]!='.'){
+                    if(board[x][y] == 'O') white = 1;
+                    if(board[x][y] == 'X') black = 1;
+                }
+            }
+        }
+    }
+    return {black, white};
+}
+
+void solve(lli tc){
+    int black = 0, white = 0;
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
+            char c; cin >> c;
+            board[i][j] = c;
+            if(board[i][j]=='O') white++;
+            if(board[i][j]=='X') black++;
+        }
+    }
+
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
+            if(board[i][j]=='.'){
+                mem(vis, 0);
+                pair<int, int> f = BFS(i, j);
+                if(f.ff and !f.ss) black++;
+                if(!f.ff and f.ss) white++;
+            }
+        }
+    }
+    print("Black " << black << " White " << white)
+}
+
+int main(){
+    ANUPAM_AKIB;
+    #ifndef ONLINE_JUDGE
+        clock_t tStart = clock();
+        INPUT;
+        OUTPUT;
+    #endif
+
+    lli tc = 1;
+
+    cin >> tc; //TEST CASE
+
+    for(int i=1; i<=tc; i++){
+        solve(i);
+    } //*/
+
+    /*while(cin >> tc && tc!=0) solve(tc);  //EOF*/
+
+    #ifndef ONLINE_JUDGE
+        execution_time;
+    #endif
+    return EXIT_SUCCESS;
+}
